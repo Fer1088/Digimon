e906dbdedf06ee2edea291e9239c2723e5601c2b
tigermafia28 <josillmoy@alu.edu.gva.es>
2022-03-10 12:26:43 +0100

    Usuario (falta constructor)

--- Digimon/src/digimon/Usuario.java
+++ Digimon/src/digimon/Usuario.java
@@ -10,4 +10,40 @@ package digimon;
  */
 public class Usuario {
     
+    private String nomUsu;
+    private String contUsu;
+    private String idUsu;
+    private int partidasGan;
+    
+    public String getNomUsu() {
+        return nomUsu;
+    }
+
+    public void setNomUsu(String nomUsu) {
+        this.nomUsu = nomUsu;
+    }
+
+    public String getContUsu() {
+        return contUsu;
+    }
+
+    public void setContUsu(String contUsu) {
+        this.contUsu = contUsu;
+    }
+
+    public String getIdUsu() {
+        return idUsu;
+    }
+
+    public void setIdUsu(String idUsu) {
+        this.idUsu = idUsu;
+    }
+
+    public int getPartidasGan() {
+        return partidasGan;
+    }
+
+    public void setPartidasGan(int partidasGan) {
+        this.partidasGan = partidasGan;
+    }
 }
