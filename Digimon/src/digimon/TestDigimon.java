e7a6dad4466da182ae8dad99d0f944468d764627
Fer1088 <fergargil@alu.edu.gva.es>
2022-03-15 09:25:59 +0100

    mevuelvoloco2

--- Digimon/src/digimon/TestDigimon.java
+++ Digimon/src/digimon/TestDigimon.java
@@ -4,10 +4,36 @@
  */
 package digimon;
 
+import static digimon.Digimon.Tipos.ANIMAL;
+import java.util.HashSet;
+
 /**
  *
  * @author ferna
  */
 public class TestDigimon {
-    
+
+    static HashSet<Digimon> digimons = new HashSet<>();
+
+    public static void creadigimons(String Nombre, Digimon.Tipos t, int lvl, int ataq, int def) {
+        Digimon tusmuertos = new Digimon(Nombre, t, lvl, ataq, def);
+
+        boolean noveas = digimons.contains(tusmuertos);
+
+        if (!noveas) {
+            digimons.add(tusmuertos);
+        } else {
+            System.out.println("Ya hay un digimon con ese nombre");
+        }
+        System.out.println(noveas);
+    }
+
+
+public static void main(String[] args){
+
+creadigimons("Felipe",ANIMAL,69,23,32);
+creadigimons("Felipe",ANIMAL,69,23,32);
+    System.out.println(digimons);
+   
+}
 }
