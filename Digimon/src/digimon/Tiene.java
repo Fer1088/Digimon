167dff1ed47125ccb9220584f6d48c77a856a316
Fer1088 <fergargil@alu.edu.gva.es>
2022-03-15 09:32:51 +0100

    entregar

---
+++ Digimon/src/digimon/Tiene.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package digimon;
+
+/**
+ *
+ * @author ferna
+ */
+public class Tiene {
+    private boolean estasiono;
+    public Tiene(boolean t){
+    estasiono=t;
+    }
+
+    public boolean isEstasiono() {
+        return estasiono;
+    }
+
+    public void setEstasiono(boolean estasiono) {
+        this.estasiono = estasiono;
+    }
+}
